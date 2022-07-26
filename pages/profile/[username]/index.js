ab8e2b79708a64fbef78c032b97f5ae553a7abb4
Bright <monirehghandani@gmail.com>
2022-07-26 12:32:33 +0430

    call api

--- pages/profile/[username]/index.js
+++ pages/profile/[username]/index.js
@@ -1,10 +1,22 @@
 import Head from "next/head";
-import classes from '../../../styles/Home.module.css';
 import {useRouter} from "next/router";
+import {useEffect, useState} from "react";
+import classes from '../../../styles/Home.module.css';
 
 export default function UserDetails() {
     const router=useRouter();
     const username=router.query.username;
+
+    const [data, setData]=useState();
+
+    const getUserDetails=async () => {
+        let res=await fetch("https://api.github.com/users/Brighttttttt");
+        return await res.json();
+    };
+
+    useEffect(() => {
+        getUserDetails().then((res) => setData(res));
+    }, []);
 
     return (
         <div className={classes.container}>
@@ -14,6 +26,8 @@ export default function UserDetails() {
 
             <main className={classes.main}>
                 <p>{username}</p>
+                {console.log(data)}
+                <div>{`${data?.id} ${data?.name}`}</div>
             </main>
         </div>
     )
